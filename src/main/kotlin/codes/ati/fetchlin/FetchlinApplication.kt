17ec3070be8898b38648c5e6f5298b3003d003d5
Attila Papp <papp.attila971@gmail.com>
2019-09-29 12:06:04 +0200

    Add empty spring app

---
+++ src/main/kotlin/codes/ati/fetchlin/FetchlinApplication.kt
@@ -0,0 +1,11 @@
+package codes.ati.fetchlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FetchlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<FetchlinApplication>(*args)
+}
