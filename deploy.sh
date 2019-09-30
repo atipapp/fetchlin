0f0d9c0ace1878be65ccf538f70d6e2f2d87f7fa
Attila Papp <papp.attila971@gmail.com>
2019-09-30 16:00:12 +0200

    Fix permissions

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ set -e
 docker build -t fetchlin/backend:$TRAVIS_COMMIT .
 docker tag fetchlin/backend:$TRAVIS_COMMIT fetchlin/backend:latest
 
+docker push fetchlin/backend:$TRAVIS_COMMIT
 docker push fetchlin/backend:latest
