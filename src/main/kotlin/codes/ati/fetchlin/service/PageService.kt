02d1529c93d1fceb3439c7f40228bf9c965b9801
Attila Papp <papp.attila971@gmail.com>
2019-10-02 18:13:41 +0200

    Implement page creation

--- src/main/kotlin/codes/ati/fetchlin/service/PageService.kt
+++ src/main/kotlin/codes/ati/fetchlin/service/PageService.kt
@@ -1,9 +1,13 @@
 package codes.ati.fetchlin.service
 
+import codes.ati.fetchlin.domain.Page
 import org.springframework.stereotype.Service
 
 @Service
 class PageService {
 
+    fun createPage(page: Page): Page {
+        return page
+    }
 
 }
