ac4391b9cae05fcc051a284a29c210ae98d1f42c
Attila Papp <papp.attila971@gmail.com>
2019-10-02 18:19:12 +0200

    Add test for getting a page

--- src/test/kotlin/codes/ati/fetchlin/service/PageServiceTest.kt
+++ src/test/kotlin/codes/ati/fetchlin/service/PageServiceTest.kt
@@ -4,6 +4,7 @@ import codes.ati.fetchlin.domain.Page
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 
 object PageServiceTest {
@@ -29,6 +30,28 @@ object PageServiceTest {
 
         val actual = service.createPage(expected)
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Get pages returns the saved entry`() {
+        val createdPage = withOnePage()
+
+        val pages = service.getPages()
+        assertTrue { pages.contains(createdPage) }
+    }
+
+    private fun withOnePage(): Page {
+        val page = Page(
+                id = "ABCD-1234",
+                url = "http://david-hasselhoff.com",
+                name = "David Hasselhoff's home page",
+                interval = 10,
+                domElement = "",
+                maxNumberOfRevisions = 10,
+                revisions = mutableListOf()
+        )
+
+        return service.createPage(page)
     }
 
 }
